f768dc85e934927ff2b9ce7f050d4f56acf50960
Sabin Park <C16Sabin.Park@usafa.edu>
2014-09-15 16:44:19 -0600

    added the skeleton file

---
+++ code/6_lab2.asm
@@ -0,0 +1,73 @@
+;-------------------------------------------------------------------------------
+; Title:		Lab 2 - Cryptography
+; Name:			C2C Sabin Park, USAF
+; Date:			15 September 2014
+; Instructor:	Dr. Coulston
+;
+; This program uses subroutines to decrypt and encrypt messages
+;-------------------------------------------------------------------------------
+
+	.cdecls C,LIST,"msp430.h"	; BOILERPLATE	Include device header file
+ 	.text						; BOILERPLATE	Assemble into program memory
+	.retain						; BOILERPLATE	Override ELF conditional linking and retain current section
+	.retainrefs					; BOILERPLATE	Retain any sections that have references to current section
+	.global main				; BOILERPLATE	Project -> Properties and select the following in the pop-up
+								; Build -> Linker -> Advanced -> Symbol Management
+								;    enter main into the Specify program entry point... text box
+
+;-------------------------------------------------------------------------------
+;           Main
+;-------------------------------------------------------------------------------
+main:
+	mov.w   #__STACK_END,SP			; BOILERPLATE	Initialize stackpointer
+	mov.w   #WDTPW|WDTHOLD,&WDTCTL 	; BOILERPLATE	Stop watchdog timer
+
+; load registers with necessary info for decryptMessage here
+
+            call    #decryptMessage
+
+forever:    jmp     forever
+
+;-------------------------------------------------------------------------------
+;           Subroutines
+;-------------------------------------------------------------------------------
+;Subroutine Name: decryptMessage
+;Author:
+;Function: Decrypts a string of bytes and stores the result in memory.  Accepts
+;           the address of the encrypted message, address of the key, and address
+;           of the decrypted message (pass-by-reference).  Accepts the length of
+;           the message by value.  Uses the decryptCharacter subroutine to decrypt
+;           each byte of the message.  Stores theresults to the decrypted message
+;           location.
+;Inputs:
+;Outputs:
+;Registers destroyed:
+;-------------------------------------------------------------------------------
+
+decryptMessage:
+
+            ret
+
+;-------------------------------------------------------------------------------
+;Subroutine Name: decryptCharacter
+;Author:
+;Function: Decrypts a byte of data by XORing it with a key byte.  Returns the
+;           decrypted byte in the same register the encrypted byte was passed in.
+;           Expects both the encrypted data and key to be passed by value.
+;Inputs:
+;Outputs:
+;Registers destroyed:
+;-------------------------------------------------------------------------------
+
+decryptCharacter:
+
+            ret
+
+;-------------------------------------------------------------------------------
+;           System Initialization
+;-------------------------------------------------------------------------------
+	.global __STACK_END				; BOILERPLATE
+	.sect 	.stack					; BOILERPLATE
+	.sect   ".reset"				; BOILERPLATE		MSP430 RESET Vector
+	.short  main					; BOILERPLATE
+

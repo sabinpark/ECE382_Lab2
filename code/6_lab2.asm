a44568c03849f01b94fee22abf925a76e6e731f0
Sabin Park <C16Sabin.Park@usafa.edu>
2014-09-16 23:31:53 -0600

    B functionality is working

--- code/6_lab2.asm
+++ code/6_lab2.asm
@@ -20,9 +20,11 @@
 	.text
 
 encrypt_address:
-	.byte	0xef,0xc3,0xc2,0xcb,0xde,0xcd,0xd8,0xd9,0xc0,0xcd,0xd8,0xc5,0xc3,0xc2,0xdf,0x8d,0x8c,0x8c,0xf5,0xc3,0xd9,0x8c,0xc8,0xc9,0xcf,0xde,0xd5,0xdc,0xd8,0xc9,0xc8,0x8c,0xd8,0xc4,0xc9,0x8c,0xe9,0xef,0xe9,0x9f,0x94,0x9e,0x8c,0xc4,0xc5,0xc8,0xc8,0xc9,0xc2,0x8c,0xc1,0xc9,0xdf,0xdf,0xcd,0xcb,0xc9,0x8c,0xcd,0xc2,0xc8,0x8c,0xcd,0xcf,0xc4,0xc5,0xc9,0xda,0xc9,0xc8,0x8c,0xde,0xc9,0xdd,0xd9,0xc5,0xde,0xc9,0xc8,0x8c,0xca,0xd9,0xc2,0xcf,0xd8,0xc5,0xc3,0xc2,0xcd,0xc0,0xc5,0xd8,0xd5,0x8f
+	.byte	0xf8,0xb7,0x46,0x8c,0xb2,0x46,0xdf,0xac,0x42,0xcb,0xba,0x03,0xc7,0xba,0x5a,0x8c,0xb3,0x46,0xc2,0xb8,0x57,0xc4,0xff,0x4a,0xdf,0xff,0x12,0x9a,0xff,0x41,0xc5,0xab,0x50,0x82,0xff,0x03,0xe5,0xab,0x03,0xc3,0xb1,0x4f,0xd5,0xff,0x40,0xc3,0xb1,0x57,0xcd,0xb6,0x4d,0xdf,0xff,0x4f,0xc9,0xab,0x57,0xc9,0xad,0x50,0x80,0xff,0x53,0xc9,0xad,0x4a,0xc3,0xbb,0x50,0x80,0xff,0x42,0xc2,0xbb,0x03,0xdf,0xaf,0x42,0xcf,0xba,0x50,0x8f
 key_address:
-	.byte	0xac
+	.byte	0xac,0xdf,0x23
+terminate_address:
+	.byte	0x00		; arbitrarily chosen value
 ;-------------------------------------------------------------------------------
 ;           Main
 ;-------------------------------------------------------------------------------
@@ -35,8 +37,10 @@ main:
 	mov.w	#encrypt_address, R4
 	mov.w	#key_address, R5
 	mov.w	#RAM, R6
+	mov.w	#terminate_address, R10
+	mov.w	#key_address, R11		; permanent holder for the key start index
 
-	call	#getLength
+	call	#getMessageLength
 
 decrypt:
 	call    #decryptMessage
@@ -45,19 +49,25 @@ forever:
 	jmp     forever
 
 
-;------------------------------------------
-;			Subroutine for message length
-;------------------------------------------
-getLength:
+;-------------------------------------------------------------------------------
+;           Subroutines
+;-------------------------------------------------------------------------------
+;Subroutine Name: getMessageLength
+;Author: C2C Sabin Park
+;Function: obtains the length of the message by subtracting the address of
+;		the key (which follows immediately after the message) from where the
+;		the message begins
+;Inputs:
+;Outputs:
+;Registers destroyed:
+;-------------------------------------------------------------------------------
+
+getMessageLength:
 	mov.w	#key_address, R8
 	mov.w	#encrypt_address, R9
 	sub.w	R9, R8		; R8 = R8-R9,  thus R8 is the length of the message
 	ret
 
-
-
-;-------------------------------------------------------------------------------
-;           Subroutines
 ;-------------------------------------------------------------------------------
 ;Subroutine Name: decryptMessage
 ;Author: C2C Sabin Park
@@ -75,6 +85,9 @@ getLength:
 decryptMessage:
 			tst			R8
 			jz			forever
+			cmp.w		#terminate_address, R5
+			jz			resetKeyIndex
+continueDecrypt:
 			mov.b		@R4+, R7	; put value at R4 into R7, increment R4
 			call		#decryptCharacter
 			mov.b		R7, 0(R6)	; put decrypted value into the address R6 points to
@@ -89,14 +102,19 @@ decryptMessage:
 ;Function: Decrypts a byte of data by XORing it with a key byte.  Returns the
 ;           decrypted byte in the same register the encrypted byte was passed in.
 ;           Expects both the encrypted data and key to be passed by value.
-;Inputs: value at R4 (encrypted), value at R5 (key)
-;Outputs: updated R4 (decrypted)
+;Inputs: value of R7 (encrypted), value at R5 (key)
+;Outputs: updated R7 (decrypted)
 ;Registers destroyed:
 ;-------------------------------------------------------------------------------
 
 decryptCharacter:
-			xor.b		@R5, R7
+			xor.b		@R5+, R7
+
             ret
+;-------------------------------------------------------------------------------
+resetKeyIndex:
+	mov.w	R11, R5
+	jmp		continueDecrypt
 
 ;-------------------------------------------------------------------------------
 ;           System Initialization
